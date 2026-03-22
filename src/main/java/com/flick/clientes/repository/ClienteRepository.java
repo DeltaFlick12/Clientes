6945786d6e9660a80f184397a8f27d497f2522d9
DeltaFlick12 <pedrolucasgomesbr1212@gmail.com>
2026-03-22 20:45:26 -0300

    repository criado

---
+++ src/main/java/com/flick/clientes/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.flick.clientes.repository;
+
+import com.flick.clientes.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
