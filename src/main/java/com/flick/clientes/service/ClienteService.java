355c5d167065914e4b04590616f5a41f826d0cc8
DeltaFlick12 <pedrolucasgomesbr1212@gmail.com>
2026-03-22 20:44:59 -0300

    service criado

---
+++ src/main/java/com/flick/clientes/service/ClienteService.java
@@ -0,0 +1,32 @@
+package com.flick.clientes.service;
+
+import com.flick.clientes.model.Cliente;
+import com.flick.clientes.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Cliente criarCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> listarClientes() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> buscarClientePorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public void deletarCliente(Long id) {
+        clienteRepository.deleteById(id);
+    }
+}
